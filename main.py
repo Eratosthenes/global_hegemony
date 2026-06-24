ea979e4f4e63e0ba2be2bc3be2a51830ed376600
Nathan Rapport <n.rapport@gmail.com>
2026-06-24 14:20:29 -0400

    reordering

--- main.py
+++ main.py
@@ -5,40 +5,40 @@ from global_hegemony.tournament import Tournament, TournamentEntry, TournamentMo
 def main() -> None:
     entries = [
         TournamentEntry.from_player_class(
-            "Spider",
-            DisciplinedAmbush,
-        ),
-        TournamentEntry.from_player_class(
-            "Paperclip Maximizer",
-            AlwaysCooperate,
-        ),
-        TournamentEntry.from_player_class(
-            "SpaceBeam",
-            CycleDefector,
-        ),
-        TournamentEntry.from_player_class(
-            "Raider",
-            RaidAndHeal,
-        ),
-        TournamentEntry.from_player_class(
-            "Tit for Tat",
-            TitForTat,
-        ),
-        TournamentEntry.from_player_class(
             "Warrior",
             AlwaysDefect,
+        ),
+        TournamentEntry.from_player_class(
+            "Spider",
+            DisciplinedAmbush,
         ),
         TournamentEntry.from_player_class(
             "Strongman",
             AntiAmbush,
         ),
         TournamentEntry.from_player_class(
+            "Raider",
+            RaidAndHeal,
+        ),
+        TournamentEntry.from_player_class(
+            "SpaceBeam",
+            CycleDefector,
+        ),
+        TournamentEntry.from_player_class(
+            "Paperclip Maximizer",
+            AlwaysCooperate,
+        ),
+        TournamentEntry.from_player_class(
             "Forager",
             Forager,
         ),
+        TournamentEntry.from_player_class(
+            "Tit for Tat",
+            TitForTat,
+        ),
     ]
 
-    tournament = Tournament(entries, mode=TournamentMode.FRESH)
+    tournament = Tournament(entries, mode=TournamentMode.PERSISTENT)
     tournament.run()
     tournament.print_summary()
 
