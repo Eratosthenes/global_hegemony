66550bdafdb4889e999b62780c4fedf4c66c29b0
Nathan Rapport <n.rapport@gmail.com>
2026-06-25 11:11:00 -0400

    strongman changes

--- global_hegemony/strategies/anti_ambush.py
+++ global_hegemony/strategies/anti_ambush.py
@@ -7,39 +7,39 @@ class AntiAmbush(Player):
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self._striking = False
+        self._campaign = False # whether the player is currently in a campaign (attacking)
 
     def choose_action(self, view: GameView) -> Action:
         # be the first to cooperate if caught in mutual defection
         # this allows self-coexistence
         if view.opponent_previous_action is Action.DEFECT and view.own_previous_action is Action.DEFECT:
-            self._striking = False
+            self._campaign = False
             return Action.COOPERATE
 
         # we can bankrupt our opponent, so defect
         if abs(view.own_d) * view.own_d >= view.opponent_capital:
-            self._striking = True
+            self._campaign = True
             return Action.DEFECT
 
         # attack if we are armed and we'll win under mutual defection
         # bias toward defection to win more frequently
         if view.own_d > 0 and view.own_d >= view.opponent_d:
-            self._striking = True
+            self._campaign = True
             return Action.DEFECT
 
         # if we are fully armed, there's no point in cooperating
         if view.own_c <= 0:
-            self._striking = True
+            self._campaign = True
             return Action.DEFECT
         
         # if we are unarmed, focus on building up our capital
         if view.own_d <= 0:
-            self._striking = False
+            self._campaign = False
 
         return Action.COOPERATE
 
     def choose_self_modification(self, view):
-        if self._striking:
+        if self._campaign:
             # if we are striking, we need to rearm
             return Modification.INCREASE_D
 
@@ -50,8 +50,8 @@ class AntiAmbush(Player):
         # they are attacking us
         if view.opponent_previous_action is Action.DEFECT:
             # resist attacks
-            self._striking = False
+            self._campaign = False
             return Modification.INCREASE_C
-
+        
         # we are attacking; remain unopinionated
         return Modification.NO_CHANGE
